f6dcbb1a98a49e08994f64ab3b88e3b397e69cac
loagexd <147046799+loagexd@users.noreply.github.com>
2023-10-05 22:17:31 +0300

    3a6.c

--- 3a6.c
+++ 3a6.c
@@ -2,9 +2,13 @@
 
 int main (void)
 {
-    int h = 5, w = 5, wm = w, f = 0, count = 0, hh;
+    int h, w, f = 0, count = 0, hh;
     char a = 'A', b = a;
-        for (hh = 0; hh <= h/2; hh++)
+    printf("Введите высоту и ширину: ");
+    scanf("%d %d",&h, &w);
+    printf("\n");
+    int wm = w, g = ((h+1)/2);
+        for (hh = 0; hh < (h+1)/2; hh++)
             {
                 f = hh;
                 for (f ; f > 0; f -= 1 )
@@ -28,21 +32,19 @@ int main (void)
                 }
                 wm -= 2; count = 0; printf("\n");
             }
-        wm += 4;
         if (h % 2 == 0)
-                {
-                    f = (h/2 +1);
-                    wm += 2;
-                }
-         else
-                    f = (h/2 +1) + 1;       
-        for (hh = h/2 + 1; hh > h/2 && hh < h; hh++)
+            wm += 2; 
+        else
+            wm += 4;      
+        for (hh = g; hh >= (h+1)/2 && hh < h; hh++)
             {   
-                for (f ; (h - f) > 0; f += 1)
+                f = hh + 1;
+                for (f; f < h; f ++)
                 {
                     printf (" ");
                     count ++;
                 } 
+                
                 b = a + count;
                 for (int ww = 0; ww < wm ; ww++)
                 {
@@ -59,4 +61,5 @@ int main (void)
                 }
                 wm += 2; count = 0; printf("\n");
             }
+    printf("\n");        
 }
